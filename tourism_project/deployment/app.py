74814ec7bacbc2f72ca92d30fceb14dfd78b8b2b
gagangoel1991 <gagangoel1991@gmail.com>
2025-11-30 03:32:23 +0000

    second commit

---
+++ tourism_project/deployment/app.py
@@ -0,0 +1,67 @@
+import streamlit as st
+import pandas as pd
+from huggingface_hub import hf_hub_download
+import joblib
+
+# Download and load the trained model
+model_path = hf_hub_download(repo_id="ggoel1991/tourism_project", filename="best_tourism_model_v1.joblib")
+model = joblib.load(model_path)
+
+# Streamlit UI
+st.title("Wellness Tourisim Prediction")
+st.write("""
+This predicts whether a customer will purchase the newly introduced Wellness Tourism Package before contacting them based on the
+parameters like Age, Duration of Pitch, Monthly Income,etc.
+Please enter the app details below to get a revenue prediction.
+""")
+
+# User input
+age_category = st.selectbox("Age Category", ["Teen", "Young Adult", "Adult", "Middle Age", "Senior"])
+duration_category= st.selectbox("Duration of Pitch", ["Very Short", "Short","Medium","Long","Very Long"])
+income_category= st.selectbox("Income Category", ["Low", "Lower-Middle","Middle","Upper-Middle","High"])
+typeof_contact = st.selectbox("Type of Contact", ["Company Invited", "Self Contacted"])
+city_tier = st.selectbox("City Tier", [1, 2, 3])
+occupation = st.selectbox("Occupation", ["Free Lancer", "Small Business", "Large Business", "Salaried"])
+gender = st.selectbox("Gender", ["Male", "Female"])
+number_of_person_visiting = st.number_input("Number of Persons Visiting", min_value=1, max_value=10, value=2)
+number_of_followups = st.number_input("Number of Followups", min_value=0, max_value=10, value=2)
+product_pitched = st.selectbox("Product Pitched", ["Basic", "Standard", "Deluxe", "Super Deluxe", "King"])
+preferred_property_star = st.selectbox("Preferred Property Star", [3, 4, 5])
+marital_status = st.selectbox("Marital Status", ["Single", "Married", "Divorced","Unmarried"])
+number_of_trips = st.number_input("Number of Trips", min_value=0, max_value=20, value=1)
+passport = st.selectbox("Passport", ["0", "1"])
+pitch_satisfaction_score = st.slider("Pitch Satisfaction Score", 1, 2,3,4,5)
+own_car = st.selectbox("Own Car", ["1", "0"])
+number_of_person_visiting = st.number_input("Number of Persons Visiting", min_value=0, max_value=10, value=2)
+designation = st.selectbox("Designation", ["Executive", "Manager", "Senior Manager", "AVP", "VP"])
+
+
+
+
+# Assemble input into DataFrame
+input_data = pd.DataFrame([{
+    'Age_cat': age_category,
+    'Duration_cat': duration_category,
+    'Income_cat': income_category,
+    'TypeofContact': typeof_contact,
+    'CityTier': city_tier,
+    'Occupation': occupation,
+    'Gender': gender,
+    'ProductPitched': product_pitched,
+    'MaritalStatus': marital_status,
+    'Designation': designation,
+    'PreferredPropertyStar': preferred_property_star,
+    'OwnCar': own_car,
+    'Passport': passport,
+    'PitchSatisfactionScore': pitch_satisfaction_score,
+    'NumberOfPersonVisiting': number_of_person_visiting,
+    'NumberOfFollowups': number_of_followups,
+    'NumberOfTrips': number_of_trips
+}])
+
+
+# Predict button
+if st.button("Predict"):
+    prediction = model.predict(input_data)[0]
+    st.subheader("Prediction Result:")
+    st.success(f"Travel Taken: **{prediction:,.2f}")
---
+++ tourism_project/hosting/hosting.py
@@ -0,0 +1,10 @@
+from huggingface_hub import HfApi
+import os
+
+api = HfApi(token=os.getenv("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"))
+api.upload_folder(
+    folder_path="tourism_project/deployment",     # the local folder containing your files
+    repo_id="ggoel1991/tourism_project",          # the target repo
+    repo_type="space",                      # dataset, model, or space
+    path_in_repo="",                          # optional: subfolder path inside the repo
+)
